9ad13d97194585c4b0fe6a64d91f0d0317be8beb
Tatsiana <tptushko@my.centennialcollege.ca>
2022-03-05 11:14:23 -0500

    spawn point at position of start tile

--- Lab7/Assets/[Scripts]/MapGenerator.cs
+++ Lab7/Assets/[Scripts]/MapGenerator.cs
@@ -9,7 +9,11 @@ public class MapGenerator : MonoBehaviour
     public GameObject startTile;
     public GameObject goalTile;
 
-    [Header("Map Properties")]
+    [Header("Spawn Point")]
+    public GameObject spawnPoint;
+    private GameObject sPoint;
+
+   [Header("Map Properties")]
 
     [Range(3, 30)]
     public int width =3;
@@ -23,27 +27,29 @@ public class MapGenerator : MonoBehaviour
 
     private int startWidth;
     private int startDepth;
+    
 
     // Start is called before the first frame update
     void Start()
     {
      startWidth =width;
      startDepth =depth;
-    BuildMap();
+     BuildMap();
+
     }
         
     // Update is called once per frame
     void Update()
     {
         if (width!=startWidth || startDepth != depth)
-        {
+        {   
             ResetMap();
             BuildMap();
         }
     }
 
     public void ResetMap()
-    {
+    {  
         startWidth = width;
         startDepth = depth;
         var size = tiles.Count;
@@ -53,6 +59,7 @@ public class MapGenerator : MonoBehaviour
             Destroy(tiles[i]);
         }
         tiles.Clear();//remove all tiles
+        Destroy(sPoint);
     }
 
     public void BuildMap()
@@ -67,17 +74,23 @@ public class MapGenerator : MonoBehaviour
         var randomGoalRowPosition = Random.Range(1, depth+1);
         var randomGoalColPosition = Random.Range(1, width+1);
 
-        while (
-            (randomGoalRowPosition == randomStartRowPosition  && (randomGoalColPosition == randomStartColPosition 
-            || randomGoalColPosition ==randomStartColPosition-1 || randomGoalColPosition == randomStartColPosition + 1)
-            )
-            || (randomGoalRowPosition == randomStartRowPosition-1 && (randomGoalColPosition == randomStartColPosition
-                || randomGoalColPosition == randomStartColPosition - 1 || randomGoalColPosition == randomStartColPosition + 1)
-            )
-            || (randomGoalRowPosition == randomStartRowPosition + 1 && (randomGoalColPosition == randomStartColPosition
-               || randomGoalColPosition == randomStartColPosition - 1 || randomGoalColPosition == randomStartColPosition + 1)
-            )
-            )
+          //check if start and goal tiles positions are the same or adjacent
+        while(  
+            (randomGoalRowPosition == randomStartRowPosition  &&              //check the same row   AND
+            (randomGoalColPosition == randomStartColPosition ||               //check the same column in given row  OR
+             randomGoalColPosition ==randomStartColPosition-1 ||              //check the  previous column in given row  OR
+             randomGoalColPosition == randomStartColPosition + 1))            //check the  next column in given row
+
+            || (randomGoalRowPosition == randomStartRowPosition-1 &&           //check the previous row   AND
+            (randomGoalColPosition == randomStartColPosition ||                //check the same column in given row  OR
+            randomGoalColPosition == randomStartColPosition - 1 ||             //check the  previous column in given row  OR
+            randomGoalColPosition == randomStartColPosition + 1))              //check the  next column in given row
+
+            || (randomGoalRowPosition == randomStartRowPosition + 1 &&         //check the next row     AND
+            (randomGoalColPosition == randomStartColPosition ||                //check the same column in given row   OR
+             randomGoalColPosition == randomStartColPosition - 1 ||            //check the  previous column in given row   OR
+             randomGoalColPosition == randomStartColPosition + 1))             //check the  next column in given row
+        )
         {
             randomGoalRowPosition = Random.Range(1, depth + 1);
             randomGoalColPosition = Random.Range(1, width + 1);
@@ -92,11 +105,15 @@ public class MapGenerator : MonoBehaviour
                    // if (row == 1 && col == 1) continue;
 
                  var tilePosition = new Vector3(col * 20.0f, 0.0f, row * 20.0f) - offset;
-                    
+                 var spawnPosition = new Vector3(col * 20.0f, 5.0f, row * 20.0f) - offset;
+
                 if (row == randomStartRowPosition && col == randomStartColPosition)   //random position of the start tile
                 {
                     //place the start tile
                     tiles.Add(Instantiate(startTile, tilePosition, Quaternion.identity, parent));
+
+                    // instantiate a spawn point at position of Start tile
+                    sPoint = Instantiate(spawnPoint, spawnPosition, Quaternion.identity);
                 }
                 else if (row == randomGoalRowPosition && col == randomGoalColPosition)  //random position of the goal tile
                     { //place the goal tile
