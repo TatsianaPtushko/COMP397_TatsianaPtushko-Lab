749d3f67841271d7da7b5ce9a132b06020a2d77c
Tatsiana <tptushko@my.centennialcollege.ca>
2022-03-04 21:08:04 -0500

    new functionality to script

--- Lab7/Assets/[Scripts]/MapGenerator.cs
+++ Lab7/Assets/[Scripts]/MapGenerator.cs
@@ -53,33 +53,53 @@ public class MapGenerator : MonoBehaviour
             Destroy(tiles[i]);
         }
         tiles.Clear();//remove all tiles
-       
-        
     }
 
     public void BuildMap()
     {
         var offset = new Vector3(20.0f,0.0f,20.0f);
 
-        //place the startTile
-       tiles.Add(Instantiate(startTile, Vector3.zero, Quaternion.identity, parent));
+      //choose a random position of Start tile
+       var randomStartRowPosition = Random.Range(1, depth + 1);
+       var randomStartColPosition = Random.Range(1, width + 1);
 
         //choose a random position of Goal tile
         var randomGoalRowPosition = Random.Range(1, depth+1);
         var randomGoalColPosition = Random.Range(1, width+1);
+
+        while (
+            (randomGoalRowPosition == randomStartRowPosition  && (randomGoalColPosition == randomStartColPosition 
+            || randomGoalColPosition ==randomStartColPosition-1 || randomGoalColPosition == randomStartColPosition + 1)
+            )
+            || (randomGoalRowPosition == randomStartRowPosition-1 && (randomGoalColPosition == randomStartColPosition
+                || randomGoalColPosition == randomStartColPosition - 1 || randomGoalColPosition == randomStartColPosition + 1)
+            )
+            || (randomGoalRowPosition == randomStartRowPosition + 1 && (randomGoalColPosition == randomStartColPosition
+               || randomGoalColPosition == randomStartColPosition - 1 || randomGoalColPosition == randomStartColPosition + 1)
+            )
+            )
+        {
+            randomGoalRowPosition = Random.Range(1, depth + 1);
+            randomGoalColPosition = Random.Range(1, width + 1);
+        }
+
 
         //generate more tiles if both width & depth > 2
         for (int row = 1; row <= depth; row++)
             {  
                 for (int col = 1; col <= width; col++)
                 {
-                    if (row == 1 && col == 1) continue;
+                   // if (row == 1 && col == 1) continue;
 
-                    var tilePosition = new Vector3(col * 20.0f, 0.0f, row * 20.0f) - offset;
-
-                    if (row == randomGoalRowPosition && col == randomGoalColPosition)
-                    {
-                        //place the goal tile
+                 var tilePosition = new Vector3(col * 20.0f, 0.0f, row * 20.0f) - offset;
+                    
+                if (row == randomStartRowPosition && col == randomStartColPosition)   //random position of the start tile
+                {
+                    //place the start tile
+                    tiles.Add(Instantiate(startTile, tilePosition, Quaternion.identity, parent));
+                }
+                else if (row == randomGoalRowPosition && col == randomGoalColPosition)  //random position of the goal tile
+                    { //place the goal tile
                         tiles.Add(Instantiate(goalTile, tilePosition, Quaternion.identity, parent));
                     }
                     else
@@ -89,10 +109,6 @@ public class MapGenerator : MonoBehaviour
                         tiles.Add(Instantiate(tilePrefabs[randomPrefabIndex], tilePosition, randRotation, parent));
                     }
                 }
-           
         }
-       
-        
-
     }
 }
