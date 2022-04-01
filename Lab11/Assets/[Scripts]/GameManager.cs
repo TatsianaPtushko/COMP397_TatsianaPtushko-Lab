89fa229fa1604048b930bef77a56e4dd9fe1edf9
Tatsiana <tptushko@my.centennialcollege.ca>
2022-04-01 19:56:54 -0400

    NetworkManager and player scripts

--- Lab11/Assets/[Scripts]/GameManager.cs
+++ Lab11/Assets/[Scripts]/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,7 +8,7 @@ public class GameManager : MonoBehaviour
     private void OnGUI()
     {
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
-        if(NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+        if(!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
         {
             //StartButton
             StartButtons();
@@ -42,7 +43,7 @@ public class GameManager : MonoBehaviour
     public static void StatusLabels()
     {
         var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
-        GUILayout.Label("Transport:");
+        GUILayout.Label("Transport:"+ NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
         GUILayout.Label("Mode:" + mode);
     }
 
--- Lab11/Assets/[Scripts]/NetworkPlayerBehaviour.cs
+++ Lab11/Assets/[Scripts]/NetworkPlayerBehaviour.cs
@@ -1,20 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 public class NetworkPlayerBehaviour : NetworkBehaviour
 {
-    public float spead;
+    public float speed;
+    public MeshRenderer meshRenderer;
+
     private NetworkVariable<float> verticalPosition = new NetworkVariable<float>();
     private NetworkVariable<float> horizontalPosition = new NetworkVariable<float>();
+    private NetworkVariable<Color> materialColor = new NetworkVariable<Color>();
 
     private float localHorizontal;
     private float localVertictal;
+    private Color localColor;
 
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        materialColor.OnValueChanged += ColorOnChange;
+    
+     if (IsClient){ 
+            GetComponent<MeshRenderer>().material.color = materialColor.Value;
+     }
+    }
+
     void Start()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+        RandomSpawnPositionAndColor();
+
+        meshRenderer.material.SetColor("_Color", materialColor.Value);
+       
+/*
+        if (IsServer)
+        {
+            meshRenderer.material.SetColor("_Color", materialColor.Value);
+        }
+*/
     }
 
     // Update is called once per frame
@@ -22,43 +47,60 @@ public class NetworkPlayerBehaviour : NetworkBehaviour
     {
         if (IsServer)
         {
-            //server update
+            ServerUpdate();
         }
         if (IsClient && IsOwner)
         {
-            //client update
-            ClientUpdate();
+           ClientUpdate();
         }
     }
 
     void ServerUpdate()
     {
         transform.position = new Vector3(transform.position.x + horizontalPosition.Value,
-            transform.position.y, transform.position.z +verticalPosition.value);
+            transform.position.y, transform.position.z +verticalPosition.Value);
+   
+       if(meshRenderer.material.GetColor("_Color") != materialColor.Value)
+        {
+            meshRenderer.material.SetColor("_Color",materialColor.Value);
+        }
+    
     }
 
-    public void RandomSpawnPosition()
+    public void RandomSpawnPositionAndColor()
     {
-        var x = Random.Range(-10f, 10f);
-        var z = Random.Range(-10f, 10f);
+        var x = Random.Range(-10.0f, 10.0f);
+        var z = Random.Range(-10.0f, 10.0f);
         transform.position = new Vector3(x, 1.0f, z);
 
+        var r = Random.Range(0, 1.0f);
+        var g = Random.Range(0, 1.0f);
+        var b = Random.Range(0, 1.0f);
+       var color = new Color(r, g, b);
+        localColor = color;
+        
+      //  SetClientColorServerRpc(color);
     }
 
     public void ClientUpdate()
     {
-        var horiz = Input.GetAxis("Horizontal") * Time.deltaTime*spead;
-        var vert = Input.GetAxis("Vertical") * Time.deltaTime*spead;
+        var horiz = Input.GetAxis("Horizontal") * Time.deltaTime*speed;
+        var vert = Input.GetAxis("Vertical") * Time.deltaTime*speed;
         
         //network update
        if(localHorizontal !=horiz || localVertictal != vert)
         {
             localHorizontal = horiz;
             localVertictal = vert;
-
             //update client position on the network
             UpdateClientPositionServerRpc(horiz, vert);
         }
+
+       if(localColor != materialColor.Value)
+        {
+            SetClientColorServerRpc(localColor);
+        }
+
     }
 
     [ServerRpc]
@@ -66,6 +108,19 @@ public class NetworkPlayerBehaviour : NetworkBehaviour
     {
         horizontalPosition.Value = horiz;
         verticalPosition.Value = vert;
+    }
+
+    [ServerRpc]
+    public void SetClientColorServerRpc(Color color)
+    {
+        materialColor.Value = color;
+        meshRenderer.material.SetColor("_Color", materialColor.Value);
+       
+    }
+
+    void ColorOnChange(Color oldColor, Color newColor)
+    {
+        GetComponent<MeshRenderer>().material.color = materialColor.Value;
     }
 
 }
