9ce435cd015f796d8a3746495acea352e6fe8a44
Tatsiana <tptushko@my.centennialcollege.ca>
2022-04-09 23:26:25 -0400

    Canvas and localCamera

--- Lab12/Assets/[Scripts]/GameManager.cs
+++ Lab12/Assets/[Scripts]/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject canvas;
+    public GameObject localCamera;
+
     private void OnGUI()
     {
         GUILayout.BeginArea(new Rect(10, 10, 300, 300));
@@ -12,6 +15,7 @@ public class GameManager : MonoBehaviour
         {
             //StartButton
             StartButtons();
+            
         }
         else
         {
@@ -22,39 +26,45 @@ public class GameManager : MonoBehaviour
         GUILayout.EndArea();
     }
 
-    public static void StartButtons()
+    public  void StartButtons()
     {
-        if (GUILayout.Button("Server"))
-        {
-            NetworkManager.Singleton.StartServer();
-        }
+        //    if (GUILayout.Button("Server"))
+        //    {
+        //        NetworkManager.Singleton.StartServer();
+        //    }
 
         if (GUILayout.Button("Host"))
         {
             NetworkManager.Singleton.StartHost();
+            TurnOffCamera();
         }
 
         if (GUILayout.Button("Client"))
         {
             NetworkManager.Singleton.StartClient();
+            TurnOffCamera();
         }
     }
 
-    public static void StatusLabels()
+    public  void StatusLabels()
     {
         var mode = NetworkManager.Singleton.IsHost ? "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
-        GUILayout.Label("Transport:"+ NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+        //GUILayout.Label("Transport:"+ NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
         GUILayout.Label("Mode:" + mode);
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public  void TurnOffCamera()
     {
-        
+        canvas.SetActive(true);
+        localCamera.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TurnOnCamera()
+    {
+        canvas.SetActive(false);
+        localCamera.SetActive(true);
+    }
+    private void Update()
     {
         
     }
